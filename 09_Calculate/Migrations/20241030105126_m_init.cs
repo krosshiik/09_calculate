c16c308572bef83dbae7f4d93639ec40ded14698
yan <ian.krasheninnikov@urfu.me>
2024-10-30 16:13:28 +0500

    Db2

---
+++ 09_Calculate/Migrations/20241030105126_m_init.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _09_Calculate.Migrations
+{
+    public partial class m_init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "DataInputVariants",
+                columns: table => new
+                {
+                    ID_DataInputVariant = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Operand_1 = table.Column<string>(type: "varchar(128)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Operand_2 = table.Column<string>(type: "varchar(128)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Type_operation = table.Column<string>(type: "varchar(128)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Result = table.Column<string>(type: "varchar(128)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataInputVariants", x => x.ID_DataInputVariant);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataInputVariants");
+        }
+    }
+}
