af1a8f4ee3157b01ca83fecf2ce7130e40fc6f4b
yan <ian.krasheninnikov@urfu.me>
2024-12-18 13:07:30 +0500

    added code for grafana and prometheus

--- 09_Calculate/Program.cs
+++ 09_Calculate/Program.cs
@@ -1,10 +1,46 @@
 using _09_Calculate.Data;
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(meterProviderBuilder =>
+    {
+        meterProviderBuilder.AddPrometheusExporter();
+
+        meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Hosting",
+                "Microsoft.AspNetCore.Server.Kestrel");
+
+        // Status code 
+        meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Http.Connections");
+
+        meterProviderBuilder.AddView("http.server.request.duration",
+            new ExplicitBucketHistogramConfiguration
+            {
+                Boundaries =
+                [
+                    0,
+                    0.005,
+                    0.01,
+                    0.025,
+                    0.05,
+                    0.075,
+                    0.1,
+                    0.25,
+                    0.5,
+                    0.75,
+                    1,
+                    2.5,
+                    5,
+                    7.5,
+                    10
+                ]
+            });
+    });
 
 string mariadbCS = builder.Configuration.GetConnectionString("DefaultConnection");
 
