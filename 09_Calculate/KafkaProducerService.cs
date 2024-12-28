4d3478f7e70272469833f5648dd51c9a626d2f11
yan <ian.krasheninnikov@urfu.me>
2024-12-28 11:04:59 +0500

    doc

--- 09_Calculate/KafkaProducerService.cs
+++ 09_Calculate/KafkaProducerService.cs
@@ -13,20 +13,9 @@ namespace _09_Calculate.Services
             kafkaHandle = new DependentProducerBuilder<K, V>(handle.Handle).Build();
         }
 
-        /// <summary>
-        ///     Asychronously produce a message and expose delivery information
-        ///     via the returned Task. Use this method of producing if you would
-        ///     like to await the result before flow of execution continues.
-        /// <summary>
         public Task ProduceAsync(string topic, Message<K, V> message)
             => kafkaHandle.ProduceAsync(topic, message);
 
-        /// <summary>
-        ///     Asynchronously produce a message and expose delivery information
-        ///     via the provided callback function. Use this method of producing
-        ///     if you would like flow of execution to continue immediately, and
-        ///     handle delivery information out-of-band.
-        /// </summary>
         public void Produce(string topic, Message<K, V> message, Action<DeliveryReport<K, V>> deliveryHandler = null)
             => kafkaHandle.Produce(topic, message, deliveryHandler);
 
